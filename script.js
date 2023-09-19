a1faf0b5c83d7fde62e4aa8ac5b45f4c7ded5d7e
OleJG <flucode9@gmail.com>
2023-09-18 21:42:18 -0400

    update Js

--- script.js
+++ script.js
@@ -24,3 +24,84 @@ const questions = [
     
 ];
 
+let currentQuestionIndex = 0;
+let timer;
+let timeLeft = 60; 
+let score = 0;
+let highScores = [];
+
+startBtn.addEventListener('click', startQuiz);
+
+function startQuiz() {
+    startBtn.style.display = 'none';
+    questionContainer.style.display = 'block';
+    startTimer();
+
+    showQuestion();
+}
+
+function startTimer() {
+    timer = setInterval(function () {
+        timeLeft--;
+        if (timeLeft <= 0) {
+            endQuiz();
+        }
+    }, 1000);
+}
+
+function showQuestion() {
+    if (currentQuestionIndex < questions.length) {
+        const currentQuestion = questions[currentQuestionIndex];
+
+        questionText.textContent = currentQuestion.question;
+        optionsList.innerHTML = '';
+
+        for (let i = 0; i < currentQuestion.options.length; i++) {
+            const option = document.createElement('li');
+            option.textContent = currentQuestion.options[i];
+            option.addEventListener('click', function () {
+                checkAnswer(currentQuestion, i);
+            });
+            optionsList.appendChild(option);
+        }
+    } else {
+        endQuiz();
+    }
+}
+
+function checkAnswer(question, selectedOptionIndex) {
+    if (question.answer === question.options[selectedOptionIndex]) {
+        score++;
+        feedbackContainer.textContent = 'Correct!';
+    } else {
+        timeLeft -= 10; 
+        feedbackContainer.textContent = 'Wrong!';
+    }
+
+    currentQuestionIndex++;
+    showQuestion();
+}
+
+function endQuiz() {
+    clearInterval(timer);
+    questionContainer.style.display = 'none';
+
+    const initials = prompt('Enter your initials:');
+    const scoreEntry = { initials, score };
+    highScores.push(scoreEntry);
+    highScores.sort((a, b) => b.score - a.score);
+
+    displayHighScores();
+}
+
+function displayHighScores() {
+    while (highscoresList.firstChild) {
+        highscoresList.removeChild(highscoresList.firstChild);
+    }
+    for (let i = 0; i < highScores.length; i++) {
+        const scoreEntry = highScores[i];
+        const li = document.createElement('li');
+        li.textContent = `${scoreEntry.initials}: ${scoreEntry.score}`;
+        highscoresList.appendChild(li);
+    }
+}
